7babf310fda9e42409232246efaddd51106cbef9
onlyver <1072539675@qq.com>
2023-03-10 12:29:13 +0800

    init

---
+++ LVMidBridgeModule/Classes/MyBridge.h
@@ -0,0 +1,16 @@
+//
+//  MyBridge.h
+//  LVMidBridgeModule
+//
+//  Created by LiLver on 2023/3/10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyBridge : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LVMidBridgeModule/Classes/MyBridge.m
@@ -0,0 +1,12 @@
+//
+//  MyBridge.m
+//  LVMidBridgeModule
+//
+//  Created by LiLver on 2023/3/10.
+//
+
+#import "MyBridge.h"
+
+@implementation MyBridge
+
+@end
